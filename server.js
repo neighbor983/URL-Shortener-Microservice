cd577c335a35a144b86e9ecaa98c42cbc7bf78e1
neighbor983 <kenneth.ray.baker@gmail.com>
2017-01-18 06:13:23 +0000

    set up mongodb

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 'use strict';
 
 var express = require('express');
-var app = express();
-var port = process.env.PORT || 8080;
 var path = require('path');
+var mongodb = require('mongodb').MongoClient;
+
+var mongoURL = process.env.MONGOLAB_URI;
+var port = process.env.PORT || 8080;
+
+var app = express();
+
+
+mongodb.connect(mongoURL, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to', mongoURL);
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
+});
+
 
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname + '/index.html'));
